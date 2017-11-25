9c5580cd5a7ef50bb5efbf82307630bcdb97d487
Dylan Katz <muddmaker.97@gmail.com>
2017-11-25 15:23:54 -0800

    AAAAAAAAAAA

--- src/SuperBlock.java
+++ src/SuperBlock.java
@@ -51,7 +51,7 @@ public class SuperBlock
     {
         for(short i = 0; i < 32; ++i) {
             if(freeList[i] != 0xFFFFFFFF) {
-                short j = i * 32;
+                short j = i * (short)32;
                 for(short k = j; k < (j>968?1000:j+32); ++k)
                     if(!getFreeList(k))
                         return k;
